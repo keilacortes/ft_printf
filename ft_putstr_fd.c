0bdfdeb719bfc1574b868ec0f50a64eb21287346
Keila Queiroz Cortes <kqueiroz@c1r3p1.42sp.org.br>
2025-08-24 20:19:59 -0300

    fix: if str == null

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: kqueiroz <kqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 22:18:48 by kqueiroz          #+#    #+#             */
-/*   Updated: 2025/08/24 17:25:00 by kqueiroz         ###   ########.fr       */
+/*   Updated: 2025/08/24 20:18:58 by kqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int	ft_putstr_fd(char *s, int fd)
 {
 	int	i;
 
-	if (!s || fd < 0)
+	if (fd < 0)
 		return (0);
+	if (!s)
+		return (write(fd, "(null)", 6));
 	i = 0;
 	while (s[i])
 	{
